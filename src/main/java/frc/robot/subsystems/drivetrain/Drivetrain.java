ab5a0324669b55218d72d66c1b944bbb1722342e
AWildKomodoGaming <thomas.trex25@gmail.com>
2026-02-24 14:36:31 -0500

    Updated with drivetrain info

--- src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.drivetrain;
 
 public class Drivetrain {
     //Drivetrain detales
+    //frame perimetre is 26in by 28in
     //6.12:1 gear ratio
     //krakens for drive moters, neos for Steering
     //need dimentions
