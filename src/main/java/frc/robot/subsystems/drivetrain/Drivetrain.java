b7b7aab3c958561c991f6090654c96536112aed9
AWildKomodoGaming <thomas.trex25@gmail.com>
2026-02-24 17:48:04 -0500

    now everyting is there

--- src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -5,4 +5,6 @@ public class Drivetrain {
     //frame perimetre is 26in by 28in
     //6.12:1 gear ratio
     //krakens for drive moters, neos for Steering
+    //wheel radius: needed
+    //distance from module centre to robot centre needed
 }
