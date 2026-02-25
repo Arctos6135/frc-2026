a952576aee5362ee278893cef5c5c3410af9b22f
EdwardW1010 <mercuryhexafluoride@gmail.com>
2026-02-25 12:59:19 -0500

    added dimension numbers for drivetrain

--- src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -5,6 +5,7 @@ public class Drivetrain {
     //frame perimetre is 26in by 28in
     //6.12:1 gear ratio
     //krakens for drive moters, neos for Steering
-    //wheel radius: needed
-    //distance from module centre to robot centre needed
-}
+    //wheel radius: 3.95 inches according to cad, may need to be checked with actual wheels
+    //distance from module centre: x: 10.375, y: 11.375, total(actual distance) = 15.3958192377 inches
+
+} 
