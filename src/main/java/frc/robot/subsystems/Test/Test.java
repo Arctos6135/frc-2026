476c42e1b3f4e1a11911e1dc5984ae1644eb03be
May <40745122+SomewhatMay@users.noreply.github.com>
2026-03-01 21:18:34 -0500

    Added a test subsystem

---
+++ src/main/java/frc/robot/subsystems/Test/Test.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.Test;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Test extends SubsystemBase{
+    Test() {
+        System.out.println(
+            "This is a test subsystem"
+        );
+    }
+}
