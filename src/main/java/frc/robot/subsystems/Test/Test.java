c527fb0a3af2a374bebd14f152e002cc3c0d68ba
Umayeer Ahsan <40745122+SomewhatMay@users.noreply.github.com>
2026-03-01 21:20:32 -0500

    Secret update

--- src/main/java/frc/robot/subsystems/Test/Test.java
+++ src/main/java/frc/robot/subsystems/Test/Test.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Test extends SubsystemBase{
     Test() {
         System.out.println(
-            "This is a test subsystem"
+            "This is a not test subsystem"
         );
     }
 }
