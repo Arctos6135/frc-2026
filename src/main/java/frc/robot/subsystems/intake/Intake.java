0ce63d9c92b9d868656ea96b623bdc81d39e4f23
Felix Huang <akafelixab@outlook.com>
2026-03-02 20:16:30 -0500

    correct getVelocity and getFilteredCurrent methods in Intake class

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.intake;
 
+import edu.wpi.first.math.filter.MedianFilter;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.intake.IntakeIO.IntakeInputs;
 
@@ -15,13 +16,13 @@ public class Intake extends SubsystemBase {
   }
 
   public double getVelocity() {
-    return inputs.
+    return inputs.velocity;
   }
   private final MedianFilter filter = new MedianFilter(IntakeConstants.MEDIAN_FILTER_SIZE);
   private double medianCurrent;
   
   public double getFilteredCurrent() {
-    
+    return medianCurrent;
   }
 
   /**
