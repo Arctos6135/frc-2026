e4ae383637ed26e789ba31cbf7f58efc8d0bd9a3
Felix Huang <akafelixab@outlook.com>
2026-03-02 20:52:03 -0500

    correct INTAKE_MOTOR constant naming and add IntakeConstants class with MEDIAN_FILTER_SIZE

--- src/main/java/frc/robot/constants/CANConstants.java
+++ src/main/java/frc/robot/constants/CANConstants.java
@@ -1,5 +1,5 @@
 package frc.robot.constants;
 
 public class CANConstants {
-  public static final int INTAKE_Motor = 0;
+  public static final int INTAKE_MOTOR = 0; // TODO
 }
---
+++ src/main/java/frc/robot/constants/IntakeConstants.java
@@ -0,0 +1,5 @@
+package frc.robot.constants;
+
+public class IntakeConstants {
+  public static final int MEDIAN_FILTER_SIZE = 0; // TODO
+}
