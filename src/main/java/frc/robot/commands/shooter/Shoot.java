7b2fd5a16282bde53a6cabea393f175bd1c9782b
EdwardW1010 <mercuryhexafluoride@gmail.com>
2026-03-05 14:14:59 -0500

    incorrect method name

--- src/main/java/frc/robot/commands/shooter/Shoot.java
+++ src/main/java/frc/robot/commands/shooter/Shoot.java
@@ -37,7 +37,8 @@ public class Shoot extends Command {
     }
 
     public void end(boolean interrupted) {
-        Shooter.setVoltage(0);
+        shooter.setVoltages(new double[]{0,0,0});
+        //Not setVoltage like previous
     }
     
 }
