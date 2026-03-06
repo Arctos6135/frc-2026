3964be814e42e2c807045d512dd6b1ac0505a0d2
NithishIsTheBest <nithish.anugithan@gmail.com>
2026-03-05 23:42:18 -0500

    Progress V2.3

--- src/main/java/frc/robot/commands/FuelIntake.java
+++ src/main/java/frc/robot/commands/FuelIntake.java
@@ -1,19 +1,20 @@
 package frc.robot.commands;
 
 import java.lang.ModuleLayer.Controller;
-
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj2.command.Command;
+public class FuelIntake extends Command {
 
-public class FuelIntake {
-
-    public double Voltage;
-
-    FuelIntake(double Voltage){
-        this.Voltage = 1;
+    public static void Intake(XboxController C, double Voltage){
+        boolean T = C.getRightTrigger();
+        if(T = true)
+        {
+            Voltage = 1;
         }
-        
-        public void setVoltage(double Voltage) {
-            this.Voltage = 0;
-        }
-        
+        else
+        {
+            Voltage = 0;
+        } 
     }
+}
