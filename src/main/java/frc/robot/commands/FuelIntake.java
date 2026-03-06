c0c760de8ff7d1f82750b71978eb3bdde8faf4a2
NithishIsTheBest <nithish.anugithan@gmail.com>
2026-03-05 20:47:31 -0500

    Const var dec v1

--- src/main/java/frc/robot/commands/FuelIntake.java
+++ src/main/java/frc/robot/commands/FuelIntake.java
@@ -2,9 +2,11 @@ package frc.robot.commands;
 
 import java.lang.ModuleLayer.Controller;
 
+import edu.wpi.first.units.measure.Voltage;
+
 public class FuelIntake {
-    FuelIntake(){
-        
+    FuelIntake(double Voltage){
+
     }
         
     }
