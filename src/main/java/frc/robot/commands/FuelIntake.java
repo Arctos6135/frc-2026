845e01ac31b515fe12fbf0b2979a2ced64107918
TimeForTrouble <immanuelroydavid29@gmail.com>
2026-03-05 20:59:27 -0500

    Progress V2.1

--- src/main/java/frc/robot/commands/FuelIntake.java
+++ src/main/java/frc/robot/commands/FuelIntake.java
@@ -5,8 +5,15 @@ import java.lang.ModuleLayer.Controller;
 import edu.wpi.first.units.measure.Voltage;
 
 public class FuelIntake {
+
+    public double Voltage;
+
     FuelIntake(double Voltage){
-        this.Voltage = 0;
+        this.Voltage = 1;
+        }
+
+        public void setVoltage(double Voltage) {
+            this.Voltage = 0;
         }
         
     }
